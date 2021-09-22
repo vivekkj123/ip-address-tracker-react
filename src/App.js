ab7b0ad9c3b3e03c11fedd9336e103099f023878
Vivek K J <vivekkj2004@gmail.com>
2021-09-22 18:59:37 +0530

    Add useEffect to detect client IP address

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import './App.scss';
-import Header from './components/Header/Header';
-import Map from './components/Map/Map';
-
+import "./App.scss";
+import Header from "./components/Header/Header";
+import Map from "./components/Map/Map";
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Map/>
+      <Header />
+      <Map />
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,46 +1,62 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import "./Header.scss";
 import arrowIcon from "./icon-arrow.svg";
 import DataBoard from "../DataBoard/DataBoard";
-import { useStateValue } from "../../StateProvider";
+import {useStateValue} from "../../StateProvider";
 import axios from "../../Axios";
 import validator from "validator";
-
 function Header() {
-  /* eslint-disable no-unused-vars */
-  const [{ data }, dispatch] = useStateValue();
-  const [Ipaddr, setIpaddr] = useState("");
-  const handleClick = async () => {
-    if (validator.isIP(Ipaddr)) {
-      let res =
-        axios.get(`api/v1?apiKey=${process.env.REACT_APP_IPIFY_APIKEY}&ipAddress=${Ipaddr}
-    `);
-    }
-    let res = await axios.get(
-      `api/v1?apiKey=${process.env.REACT_APP_IPIFY_APIKEY}&domain=${Ipaddr}`
-    );
+    useEffect( () => {
+      let getClientIpDetails =async () =>{
+        let res = await axios.get(`api/v1?apiKey=${
+          process.env.REACT_APP_IPIFY_APIKEY
+      }`);
 
-    dispatch({ type: "SET_DATA", data: res });
-  };
-  return (
-    <>
-      <div className="Header">
-        <h2>IP Address Tracker</h2>
-        <div className="inputBox">
-          <input
-            value={Ipaddr}
-            onChange={(e) => setIpaddr(e.target.value)}
-            type="text"
-            placeholder="Search for any IP address or domain"
-          />
-          <button onClick={handleClick}>
-            <img src={arrowIcon} alt="" />
-          </button>
-        </div>
-      </div>
-      <DataBoard />
-    </>
-  );
+      dispatch({type: "SET_DATA", data: res});
+      }
+      getClientIpDetails()
+    })
+
+    /* eslint-disable no-unused-vars */
+    const [
+        {
+            data
+        }, dispatch
+    ] = useStateValue();
+    const [Ipaddr, setIpaddr] = useState("");
+    const handleClick = async () => {
+        if (validator.isIP(Ipaddr)) {
+            let res = axios.get(`api/v1?apiKey=${
+                process.env.REACT_APP_IPIFY_APIKEY
+            }&ipAddress=${Ipaddr}
+    `);
+        }
+        let res = await axios.get(`api/v1?apiKey=${
+            process.env.REACT_APP_IPIFY_APIKEY
+        }&domain=${Ipaddr}`);
+
+        dispatch({type: "SET_DATA", data: res});
+    };
+    return (
+        <>
+            <div className="Header">
+                <h2>IP Address Tracker</h2>
+                <div className="inputBox">
+                    <input value={Ipaddr}
+                        onChange={
+                            (e) => setIpaddr(e.target.value)
+                        }
+                        type="text"
+                        placeholder="Search for any IP address or domain"/>
+                    <button onClick={handleClick}>
+                        <img src={arrowIcon}
+                            alt=""/>
+                    </button>
+                </div>
+            </div>
+            <DataBoard/>
+        </>
+    );
 }
 
 export default Header;
